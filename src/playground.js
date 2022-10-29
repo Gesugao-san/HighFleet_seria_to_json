46053e9cead0d8e3975cc6ef43866abca4270aad
Gesugao-san <gesugao.san@gmail.com>
2022-10-30 01:16:04 +0300

    Cann't handle it even in sandbox, silently crying

--- src/playground.js
+++ src/playground.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-let a = [];
-let s, s2 = '';
+let a, a2 = [];
+let s = '';
+let d = {};
+
 /*
 s = '{\r\n1=2\r\n{\r\n3=4\r\n}\r\n5=6\r\n6=6\r\n6=6\r\n}\r\n';
 const brackets = {
@@ -25,68 +27,58 @@ function clearArrayFromEmptyElements(array) {
 }
 
 const isSymbol = {
-  BracketIn:  (e) => {return e.includes('{');},
-  BracketOut: (e) => {return e.includes('}');},
-  KeyPair:    (e) => {return e.includes('=');}
+  KeyPair:          (e) => {return e.includes('=');},
+  SoftBracketIn:    (e) => {return e.includes('{');},
+  SoftBracketOut:   (e) => {return e.includes('}');},
+  StrictBracketIn:  (e) => {return e == '{';},
+  StrictBracketOut: (e) => {return e == '}';}
 };
 
 
 (() => {
   s = [
     '{\r\nm_classname=Node\r\nm_rnd_key1=data\r\nm_rnd_key2=data',
-    '{\r\nm_classname=Body\r\nm_rnd_key1=data\r\nm_rnd_key2=data',
-    '}\r\nm_classname=Body\r\nm_rnd_key1=data\r\nm_rnd_key2=data',
+    '{\r\nm_classname=Body\r\nm_rnd_key3=data\r\nm_rnd_key4=data',
+    '}\r\nm_classname=Body\r\nm_rnd_key5=data\r\nm_rnd_key6=data',
     '}\r\n'
   ].join('\r\n');  // SAMPLE DATA
-  console.log('s:', [JSON.stringify(s)], '\n', s);
-
-  s2 = s + '}█';
-  s2 = s2.replaceAll('\r', '').replaceAll('\n', '\r\n').replaceAll('\r\n\r\n', '\r\n');
-  s2 = s
-    .replace('{', '[[')
-    .replace('}\r\n}█', ']]')
-    .replace('}\n}█', ']]')
-    .replaceAll('{', '[')
-    .replaceAll('}', ']')
-    .replaceAll('=', '":"')
-    .replaceAll('\r\n', '"\r\n"')
-    .replaceAll('"\r\n', '",\r\n')
-    .replaceAll('[",\r\n', '[\r\n')
-    .replaceAll('\r\n"]', '\r\n]')
-    .replaceAll(']",\r\n', ']\r\n')
-    .replaceAll('\r\n"[', '\r\n[');
-
-  s2 = JSON.parse(s2);
-  console.log('s2:', s2);
-
+  console.log('s:', [JSON.stringify(s)]);
   a = s.replaceAll('\r', '').split('\n');
   a = clearArrayFromEmptyElements(a);
   console.log('a:', a);
 
   console.log('processing...');
-  let d = {};
   let depth = 0;
   let dup_num = 0;
-  let upper_key = '';
-  for (let i = 0; i < a.length; i++) {
-    const e = a[i];
-    if ([0, a.length].includes(i)) continue; // ignore depth level 0
-    if (isSymbol.BracketOut(e)) depth--;
-    if (isSymbol.BracketIn(e)) {
+  a2 = a.map((e, i) => {
+    if (i == a.length - 1) return '}\n}';
+    /* if (i == 0) {
       depth++;
-      upper_key = a[i - 1];
-    }
+      const [k, v] = a[i + 1].split('=');  // m_classname
+      return '{\n"' + v + '__' + depth + '": {';
+    } */
     if (isSymbol.KeyPair(e)) {
       const [k, v] = e.split('=');
-      if (!d['depth__' + depth]) d['depth__' + depth] = {};
-      if (!d['depth__' + depth][k]) {
-        d['depth__' + depth][k] = v;
-      } else {
-        dup_num++;
-        d['depth__' + depth][k + '__dup' + dup_num] = v;
-      }
-    };
-  }
+      return '"' + k + '": "' + v + '"' + (!isSymbol.StrictBracketOut(a[i + 1]) ? ',' : '');
+    }
+    if (isSymbol.StrictBracketIn(e)) {
+      depth++;
+      const [k, v] = a[i + 1].split('=');  // m_classname
+      return (i == 0 ? '{\n' : '') + '"' + v + '__' + depth + '": {';
+    }
+    if (isSymbol.StrictBracketOut(e)) {
+      //depth--;
+      //a[i - 1] = a[i - 1].slice(0, -1); // last space coma
+      return '},';
+    }
+    console.warn('e:', e);
+    return e;
+  });
+
+  console.log('a2:', a2.join(' '));
+  a2 = a2.join('\n');
+  d = JSON.parse(a2);
   console.log('d:', d);
+  console.log(JSON.stringify(d));
 })();
 
